c3122592d8d9bced42da0faa1445be11f6d40ec6
Георгий Воякин <voyaking@gmail.com>
2020-09-25 01:42:44 +0300

    task7 in progress 2

--- src/ru/mirea/ikbo1319/task7/FurnitureShop.java
+++ src/ru/mirea/ikbo1319/task7/FurnitureShop.java
@@ -1,4 +1,19 @@
 package ru.mirea.ikbo1319.task7;
 
+import java.util.HashMap;
+
 public class FurnitureShop {
+    private HashMap<Furniture, Double> items;
+
+    public FurnitureShop(){
+        items = new HashMap<>();
+    }
+
+    public void addItem(Furniture furniture, double price){
+        items.put(furniture, price);
+    }
+
+    public double getPrice(Furniture furniture){
+        return items.get(furniture);
+    }
 }
--- src/ru/mirea/ikbo1319/task7/Person.java
+++ src/ru/mirea/ikbo1319/task7/Person.java
@@ -4,6 +4,36 @@ import java.util.ArrayList;
 
 public class Person {
     private ArrayList<Furniture> shoppingBag;
+    private double balance;
+    private String name;
 
-    
+    public Person(ArrayList<Furniture> shoppingBag, double balance, String name) {
+        this.shoppingBag = shoppingBag;
+        this.balance = balance;
+        this.name = name;
+    }
+
+    public ArrayList<Furniture> getShoppingBag() {
+        return shoppingBag;
+    }
+
+    public void setShoppingBag(ArrayList<Furniture> shoppingBag) {
+        this.shoppingBag = shoppingBag;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
