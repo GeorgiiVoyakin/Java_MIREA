ba89d8a829d918f6978fe534eeaed787353b2bea
Георгий Воякин <voyaking@gmail.com>
2020-11-11 18:45:43 +0300

    task 19 finished

--- src/ru/mirea/ikbo1319/task19/Main.java
+++ src/ru/mirea/ikbo1319/task19/Main.java
@@ -1,20 +1,34 @@
 package ru.mirea.ikbo1319.task19;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
+    private static final String PATH = ".\\src\\ru\\mirea\\ikbo1319\\task19\\text.txt";
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
-        File file = new File(".\\src\\ru\\mirea\\ikbo1319\\task19\\text.txt");
+        File file = new File(PATH);
         file.createNewFile();
 
         FileWriter fw = new FileWriter(file);
-        fw.write(scanner.nextLine());
+        fw.append(scanner.nextLine());
 
         fw.close();
+
+        scanner = new Scanner(file);
+        System.out.println(scanner.nextLine());
+
+        scanner = new Scanner(System.in);
+
+        try(FileWriter fileWriter = new FileWriter(PATH, true);
+            BufferedWriter bw = new BufferedWriter(fileWriter);
+            PrintWriter pw = new PrintWriter(bw))
+        {
+            pw.println("\n" + scanner.nextLine());
+        } catch (IOException e) {
+            //exception handling left as an exercise for the reader
+        }
     }
 }
